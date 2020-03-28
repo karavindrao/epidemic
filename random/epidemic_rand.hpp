517de8ffbc0d8573f6da4eb71029adca9923e337
Aravind Karanam <62037160+karavindrao@users.noreply.github.com>
2020-03-28 05:46:36 -0700

    Create epidemic_rand.hpp

---
+++ random/epidemic_rand.hpp
@@ -0,0 +1,5 @@
+void init_infect(char *arr, int size, int init);
+
+void pop_count(char *arr, int size, int &sus, int &inf, int &rec);
+
+void run_update(char *arr, int size, int cont, float inf_prob, float rec_prob);
