55d2fd5740520148a307d77a13856341cde543f8
Aravind Karanam <62037160+karavindrao@users.noreply.github.com>
2020-03-28 05:49:31 -0700

    Delete epidemic_spatial.hpp

--- epidemic_spatial.hpp
+++
@@ -1,5 +0,0 @@
-void initialise(char **arr, int side, int init_infect);
-
-void pop_count(char **arr, int side, int &sus, int &inf, int &rec);
-
-void run_update(char **arr, int side, int neigh, float inf_prob, float rec_prob);
